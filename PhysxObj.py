5195be3dcf841b1318508faec9120b261f1a7197
Eugene <eugene.ususual@gmail.com>
2020-10-25 21:27:23 +0200

    test

---
+++ PhysxObj.py
@@ -0,0 +1,62 @@
+from abc import ABC
+
+
+class PhysicalObject(ABC):
+    def __init__(self):
+        self._coord = (0, 0)  # X|Y
+        self._vel = (0, 0)    # X|Y
+        self._acc = (0, 0)    # X|Y
+        self._size = (0, 0)   # H|W
+
+    @property
+    def coord(self):
+        return self._coord
+
+    @property
+    def vel(self):
+        return self._vel
+
+    @property
+    def acc(self):
+        return self._acc
+
+    @property
+    def size(self):
+        return self._size
+
+    def set_vel(self, x_vel: int, y_vel: int):
+        self._vel = (x_vel, y_vel)
+
+    def set_coord(self, x_coord: int, y_coord: int):
+        self._coord = (x_coord, y_coord)
+
+    def set_acc(self, x_acc: int, y_acc: int):
+        self._acc = (x_acc, y_acc)
+
+    def set_size(self, width: int, height: int):
+        self._acc = (width, height)
+
+    def update(self):
+        c_acc_x = self._acc[0]
+        c_acc_y = self._acc[1]
+
+        c_vel_x = self._vel[0]
+        c_vel_y = self._vel[1]
+
+        c_coord_x = self._coord[0]
+        c_coord_y = self._coord[1]
+
+        c_vel_x += c_acc_x
+        c_vel_y += c_acc_y
+
+        c_coord_x += c_vel_x
+        c_coord_y += c_vel_y
+
+        if c_coord_y < 0:
+            c_vel_y = 0
+            c_acc_y = 0
+            c_coord_y = 0
+
+        self.set_acc(c_acc_x, c_acc_y)
+        self.set_vel(c_vel_x, c_vel_y)
+        self.set_coord(c_coord_x, c_coord_y)
---
+++ prop.py
@@ -0,0 +1,55 @@
+from abc import ABC
+from PhysxObj import PhysicalObject
+from random import randint
+
+
+class Prop(PhysicalObject, ABC):
+
+    def __init__(self, height: int, width: int):
+        PhysicalObject.__init__(self)
+        self._prop_vel = 5
+        self._vel = (self._prop_vel, 0)
+        self._spawn_height = 0
+        self._size = (height, width)
+        self.is_spawned = False
+
+    @property
+    def prop_vel(self):
+        return self._prop_vel
+
+    @prop_vel.setter
+    def prop_vel(self, vel: int):
+        self._prop_vel = vel
+
+    @property
+    def spawn_height(self):
+        return self._spawn_height
+
+    @spawn_height.setter
+    def spawn_height(self, height):
+        if height <= 0:
+            self._spawn_height = 0
+        else:
+            self._spawn_height = height
+
+    def spawn(self, x: int, y: int):
+        self.set_coord(x, y)
+        self.is_spawned = True
+
+    def move(self):
+        if self.is_spawned:
+            self.update()
+
+
+class Bird(Prop):
+    spawnheight = (0, 50)
+
+    def __init__(self, height: int, width: int):
+        Prop.__init__(self, height, width)
+        self.spawn_height = Bird.spawnheight[randint(0, 1)]
+
+
+class Cactus(Prop):
+    def __init__(self, height: int, width: int):
+        Prop.__init__(self, height, width)
+        self.spawn_height = 0
