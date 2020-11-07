d501626d42619929ea256b31cf129893b64da878
Abitgor <pozitivius228@gmail.com>
2020-11-07 12:31:27 +0200

    UML and private var in GameEngine

--- Game.py
+++ Game.py
@@ -10,134 +10,134 @@ from menu import GameMenu
 
 class GameEngine:
     def __init__(self):
-        self.is_running = False
+        self.__is_running  = False
 
-        self.animation_counter = 0
-        self.waiter_counter = 0
-        self.score = 0
+        self.__animation_counter = 0
+        self.__waiter_counter = 0
+        self.__score = 0
 
-        self.clock = None
-        self.width = WIDTH
-        self.height = HEIGHT
-        self.hero = None
-        self.is_human = None
+        self.__clock = None
+        self.__width = WIDTH
+        self.__height = HEIGHT
+        self.__hero = None
+        self.__is_human = None
 
-        self.visible_obj = []
-        self.screen = pg.display.set_mode((self.width, self.height))
+        self.__visible_obj = []
+        self.screen = pg.display.set_mode((self.__width, self.__height))
 
-        self.environment = None
-        self.graphics = Graphics(self.screen)
+        self.__environment = None
+        self.__graphics = Graphics(self.screen)
 
         pg.init()
 
-    def create_level(self):
-        self.environment.spawn_prop(FIRST_SPAWN_DISTANCE)
+    def __create_level(self):
+        self.__environment.spawn_prop(FIRST_SPAWN_DISTANCE)
         for props in range(0, NUMBER_OF_EXISTING_PROP):
-            self.environment.spawn_prop()
+            self.__environment.spawn_prop()
 
-    def continue_game(self):
-        self.update()
+    def __continue_game(self):
+        self.__update()
 
     @staticmethod
-    def back_to_main_menu():
+    def __back_to_main_menu():
         menu = GameMenu(GameEngine())
         menu.start_menu()
 
-    def pause_menu(self):
+    def __pause_menu(self):
         menu = pgm.Menu(300, 400, 'Pause', theme=pgm.themes.THEME_DARK)
-        menu.add_button('Continue', self.continue_game)
-        menu.add_button('Back to main menu', self.back_to_main_menu)
+        menu.add_button('Continue', self.__continue_game)
+        menu.add_button('Back to main menu', self.__back_to_main_menu)
         menu.add_button('Exit', pgm.events.EXIT)
         menu.mainloop(self.screen)
 
-    def draw_visible_obj(self):
-        self.visible_obj = []
-        self.visible_obj.append(self.hero)
+    def __draw_visible_obj(self):
+        self.__visible_obj = []
+        self.__visible_obj.append(self.__hero)
 
-        for prop in self.environment.prop_list:
-            if prop.coord[0] < self.width - prop.size[0]:
-                self.visible_obj.append(prop)
+        for prop in self.__environment.prop_list:
+            if prop.coord[0] < self.__width - prop.size[0]:
+                self.__visible_obj.append(prop)
 
-        for obj in self.visible_obj:
-            if isinstance(self.hero, Agent):
-                self.graphics.draw_obj_without_image(obj)
+        for obj in self.__visible_obj:
+            if isinstance(self.__hero, Agent):
+                self.__graphics.draw_obj_without_image(obj)
             elif isinstance(obj, Hero):
-                self.graphics.draw_obj(obj, (self.animation_counter // 5) % 6)
+                self.__graphics.draw_obj(obj, (self.__animation_counter // 5) % 6)
             elif isinstance(obj, Cactus):
                 if not obj.is_visible:
                     obj.make_visible()
-                self.graphics.draw_obj(obj, 0)
+                self.__graphics.draw_obj(obj, 0)
             elif isinstance(obj, Bird):
                 if not obj.is_visible:
                     obj.make_visible()
-                self.graphics.draw_obj(obj, (self.animation_counter // 12) % 2)
+                self.__graphics.draw_obj(obj, (self.__animation_counter // 12) % 2)
 
     def setup(self, mode):
-        self.is_running = True
-        self.waiter_counter = 0
-        self.animation_counter = 0
-        self.environment = Environment()
+        self.__is_running = True
+        self.__waiter_counter = 0
+        self.__animation_counter = 0
+        self.__environment = Environment()
         if mode == HUMAN:
-            self.hero = Human()
-            self.is_human = True
+            self.__hero = Human()
+            self.__is_human = True
         else:
-            self.hero = Agent()
-            self.is_human = False
-        self.create_level()
-        self.clock = pg.time.Clock()
+            self.__hero = Agent()
+            self.__is_human = False
+        self.__create_level()
+        self.__clock = pg.time.Clock()
 
-        self.update()
+        self.__update()
 
-    def key_checker(self):
-        if isinstance(self.hero, Human):
+    def __key_checker(self):
+        if isinstance(self.__hero, Human):
             pressed = pg.key.get_pressed()
             key_arr = [pg.K_UP, pg.K_SPACE, pg.K_DOWN, pg.K_LCTRL]
-            self.hero.change_state(pressed, key_arr)
+            self.__hero.change_state(pressed, key_arr)
 
-    def collision_stuff(self):
+    def __collision_stuff(self):
         col_log = CollisionLogic()
 
-        cur_prop = self.environment.prop_list[0]
+        cur_prop = self.__environment.prop_list[0]
 
-        return col_log.check_collision(self.hero, cur_prop)
+        return col_log.check_collision(self.__hero, cur_prop)
 
-    def update(self):
-        while self.is_running or self.waiter_counter <= FPS * 4:
-            self.graphics.draw_background(self.animation_counter, isinstance(self.hero, Human))
+    def __update(self):
+        while self.__is_running  or self.__waiter_counter <= FPS * 4:
+            self.__graphics.draw_background(self.__animation_counter, isinstance(self.__hero, Human))
 
             for event in pg.event.get():
                 if event.type == pg.QUIT:
-                    self.is_running = False
-                    self.waiter_counter = FPS * 4
+                    self.__is_running = False
+                    self.__waiter_counter = FPS * 4
 
-            if self.collision_stuff():
-                self.is_running = False
+            if self.__collision_stuff():
+                self.__is_running = False
 
-            self.graphics.draw_text("Score:{}".format(self.score), (980, 50), (255, 255, 255))
+            self.__graphics.draw_text("__score:{}".format(self.__score), (980, 50), (255, 255, 255))
             if pg.key.get_pressed()[pg.K_ESCAPE]:
-                self.pause_menu()
+                self.__pause_menu()
 
-            if self.is_running:
-                if self.is_human:
-                    self.key_checker()
-                self.hero.update()
+            if self.__is_running :
+                if self.__is_human:
+                    self.__key_checker()
+                self.__hero.__update()
 
-                if self.animation_counter % 10 == 0:
-                    self.score += 1
+                if self.__animation_counter % 10 == 0:
+                    self.__score += 1
 
-                self.environment.update()
-                self.animation_counter += 1
+                self.__environment.__update()
+                self.__animation_counter += 1
             else:
-                self.waiter_counter += 1
-                self.graphics.draw_wasted_screen()
+                self.__waiter_counter += 1
+                self.__graphics.draw_wasted_screen()
 
                 if pg.key.get_pressed()[pg.K_SPACE]:
-                    self.back_to_main_menu()
+                    self.__back_to_main_menu()
 
-            self.draw_visible_obj()
+            self.__draw_visible_obj()
 
-            if self.animation_counter == FPS:
-                self.animation_counter = 0
+            if self.__animation_counter == FPS:
+                self.__animation_counter = 0
 
             pg.display.update()
-            self.clock.tick(FPS)
+            self.__clock.tick(FPS)
